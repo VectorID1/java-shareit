1cec0fdc9c4a2eecf0470f10f494c3f6a34cb6a9
Alexey.R <VectorID1@yandex.ru>
2025-12-05 16:08:36 +0300

    Реализация add-controllers, Добавили создание пользователя, предмета, базовые контролены для этих классов
    # Conflicts:
    #	src/main/java/ru/practicum/shareit/item/service/ItemService.java

--- src/main/java/ru/practicum/shareit/item/service/ItemService.java
+++ src/main/java/ru/practicum/shareit/item/service/ItemService.java
@@ -6,9 +6,14 @@ import java.util.List;
 
 public interface ItemService {
     Item create(Item item, Long ownerId);
+
     Item update(Long itemId, Item item, Long ownerId);
+
     Item getById(Long itemId);
+
     List<Item> getAllByOwner(Long ownerId);
+
     List<Item> search(String text);
+
     void delete(Long itemId);
 }
